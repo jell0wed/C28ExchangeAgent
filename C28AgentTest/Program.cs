619331e02fe7a910b64a09c8fe5789c5a45d84ef
Jeremie P <jeremie@weboxigen.com>
2016-03-10 20:02:48 -0500

    ...

--- C28AgentTest/Program.cs
+++ C28AgentTest/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SprintMarketing.C28.ExchangeAgent;
+using Rhesus.Agents.SprintMarketing;
 
 namespace C28AgentTest
 {
@@ -11,7 +11,7 @@ namespace C28AgentTest
     {
         static void Main(string[] args)
         {
-            var test = new C28RoutingAgent();
+            var test = new SprintRoutingAgent();
         }
     }
 }
