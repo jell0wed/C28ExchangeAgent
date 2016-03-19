fc626427ed4f66accec5ccec9bbc751c27399568
Jeremie P <jeremie@weboxigen.com>
2016-03-19 15:35:58 -0400

    test

--- SprintMarketing.C28.ExchangeAgent/C28RoutingAgent.cs
+++ SprintMarketing.C28.ExchangeAgent/C28RoutingAgent.cs
@@ -13,6 +13,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading;
 using System.Configuration.Install;
+using System.Linq.Expressions;
 using Microsoft.Win32;
 using Microsoft.Exchange.Data.Transport.Smtp;
 using SprintMarketing.C28.ExchangeAgent.converters;
@@ -29,52 +30,69 @@ namespace SprintMarketing.C28.ExchangeAgent {
 
     public class C28RoutingAgent : RoutingAgent
     {
-        private C28DecisionMaker context;
-
         public C28RoutingAgent()
         {
-            this.context = C28AgentManager.getInstance().getContext();
-
             OnResolvedMessage += SprintRoutingAgent_OnResolvedMessage;
         }
 
         void SprintRoutingAgent_OnResolvedMessage(ResolvedMessageEventSource source, QueuedMessageEventArgs e)
         {
-            if (!this.context.shouldBeHandledByC28(e.MailItem))
-            {
-                C28Logger.Info(C28Logger.C28LoggerType.AGENT, String.Format("Message from '{0}'. Domain is not present, ignoring.", e.MailItem.FromAddress.ToString()));
-                return;
-            }
-
-            C28ExchangeDomain domain = this.context.exchangeData.getDomain(e.MailItem.FromAddress.ToString());
-            RoutingAddress fromAddr = e.MailItem.FromAddress;
-            C28Logger.Debug(C28Logger.C28LoggerType.AGENT, String.Format("Domain '{0}' is set to be overriden to routing domain '{1}'", fromAddr.DomainPart, domain.connector_override));
             try
             {
-                Stream newBodyContent = e.MailItem.Message.Body.GetContentWriteStream();
-                C28MessageConverterFactory.getConverterForEmailMessage(e.MailItem.Message.Body.BodyFormat).convert(e.MailItem.Message, ref newBodyContent);
-            }
-            catch (C28ConverterException ee)
-            {
-                C28Logger.Error(C28Logger.C28LoggerType.AGENT, "Error while trying to convert message", ee);
-            }
-
-            foreach (var recp in e.MailItem.Recipients) {
-                if (fromAddr.DomainPart.ToLower() == recp.Address.DomainPart.ToLower() && domain.same_domain_action == "LocalDelivery")
+                var context = C28AgentManager.getInstance().getContext();
+                if (!context.shouldBeHandledByC28(e.MailItem))
                 {
-                    C28Logger.Debug(C28Logger.C28LoggerType.AGENT, String.Format("Message from '{0}' to '{1}' was ignored; both are on the same internal domain.", fromAddr.ToString(), recp.Address.ToString()));
-                    continue;
-                }
-                if (recp.RecipientCategory == RecipientCategory.InSameOrganization && this.context.exchangeData.currentClient.same_organization_action == "LocalDelivery") {
-                    C28Logger.Debug(C28Logger.C28LoggerType.AGENT, String.Format("Recipient '{0}' is in the same organization; ignoring.", recp.Address.ToString()));
-                    continue;
+                    C28Logger.Info(C28Logger.C28LoggerType.AGENT,
+                        String.Format("Message from '{0}'. Domain is not present, ignoring.",
+                            e.MailItem.FromAddress.ToString()));
+                    return;
                 }
 
-                recp.SetRoutingOverride(new RoutingDomain(domain.connector_override));
+                C28ExchangeDomain domain = context.exchangeData.getDomain(e.MailItem.FromAddress.ToString());
+                RoutingAddress fromAddr = e.MailItem.FromAddress;
+                C28Logger.Debug(C28Logger.C28LoggerType.AGENT,
+                    String.Format("Domain '{0}' is set to be overriden to routing domain '{1}'", fromAddr.DomainPart,
+                        domain.connector_override));
+                try
+                {
+                    Stream newBodyContent = e.MailItem.Message.Body.GetContentWriteStream();
+                    C28MessageConverterFactory.getConverterForEmailMessage(e.MailItem.Message.Body.BodyFormat)
+                        .convert(e.MailItem.Message, ref newBodyContent);
+                }
+                catch (C28ConverterException ee)
+                {
+                    C28Logger.Error(C28Logger.C28LoggerType.AGENT, "Error while trying to convert message", ee);
+                }
+
+                foreach (var recp in e.MailItem.Recipients)
+                {
+                    if (fromAddr.DomainPart.ToLower() == recp.Address.DomainPart.ToLower() &&
+                        domain.same_domain_action == "LocalDelivery")
+                    {
+                        C28Logger.Debug(C28Logger.C28LoggerType.AGENT,
+                            String.Format(
+                                "Message from '{0}' to '{1}' was ignored; both are on the same internal domain.",
+                                fromAddr.ToString(), recp.Address.ToString()));
+                        continue;
+                    }
+                    if (recp.RecipientCategory == RecipientCategory.InSameOrganization &&
+                        context.exchangeData.currentClient.same_organization_action == "LocalDelivery")
+                    {
+                        C28Logger.Debug(C28Logger.C28LoggerType.AGENT,
+                            String.Format("Recipient '{0}' is in the same organization; ignoring.",
+                                recp.Address.ToString()));
+                        continue;
+                    }
+
+                    recp.SetRoutingOverride(new RoutingDomain(domain.connector_override));
+                }
+
+                return;
             }
-
-            return;
+            catch (Exception ee)
+            {
+                C28Logger.Fatal(C28Logger.C28LoggerType.AGENT, "Unhandled Exception", ee);
+            }
         }
-        
     }
 }
